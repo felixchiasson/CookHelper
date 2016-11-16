e5ca7a9cf593a2ed89784b4271ad3a2db2d385eb
luanphung <luan_phung@rocketmail.com>
2016-11-16 12:25:13 -0500

    Modified the Main_Activity to include the buttons from the navigation bar.

--- app/src/main/java/tophaters/cookhelper/MainActivity.java
+++ app/src/main/java/tophaters/cookhelper/MainActivity.java
@@ -81,17 +81,23 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_addRecipe) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_addCategory) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_addIngredient) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_addOrigin) {
+
+        }else if (id == R.id.nav_viewCategories) {
+
+        }else if (id == R.id.nav_viewIngredients) {
+
+        }else if (id == R.id.nav_viewRecipe) {
 
         }
 
