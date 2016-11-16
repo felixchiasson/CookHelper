6351282606657d4badeee891b41e21a42ee8579e
luanphung <luan_phung@rocketmail.com>
2016-11-16 12:47:25 -0500

    Fixed Added Ingredient

--- app/src/main/java/tophaters/cookhelper/add_Ingredient.java
+++ app/src/main/java/tophaters/cookhelper/add_Ingredient.java
@@ -3,11 +3,11 @@ package tophaters.cookhelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class addRecipe extends AppCompatActivity {
+public class add_Ingredient extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_recipe);
+        setContentView(R.layout.activity_add__ingredient);
     }
 }
