f718d1f4965762c49b6df09ab8bc39431299e0db
Felix Chiasson <github@felixchiasson.ca>
2016-11-17 15:05:52 -0500

    Added a back button to add_Ingredient activity

--- app/src/main/java/tophaters/cookhelper/add_Ingredient.java
+++ app/src/main/java/tophaters/cookhelper/add_Ingredient.java
@@ -13,18 +13,6 @@ public class add_Ingredient extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__ingredient);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
